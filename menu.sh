6553e9e20cdcafc755d09912c2a28807b6927e4f
kcamesel <kaelig.camesella@gmail.com>
2025-10-02 13:15:39 +0200

    Changes to be committed: 	modified:   menu.sh

--- menu.sh
+++ menu.sh
@@ -87,23 +87,6 @@ Examples:
 EOF
 }
 
-"         _nnnn_\n" +
-"        dGGGGMMb\n" +
-"       @p~qp~~qMb\n" +
-"       M|@||@) M|\n" +
-"       @,----.JM|\n" +
-"      JS^\\__/  qKL\n" +
-"     dZP        qKRb\n" +
-"    dZP          qKKb\n" +
-"   fZP            SMMb\n" +
-"   HZM            MMMM\n" +
-"   FqM            MMMM\n" +
-" __| \".        |\\dS\"qML\n" +
-" |    `.       | `' \\Zq\n" +
-"_)      \\.___.,|     .'\n" +
-"\\____   )MMMMMP|   .'\n" +
-"     `-'       `--'"
-
 print_header() {  
   clear
   echo "╔════════════════════════════════════════════════════════════════════════════╗"
